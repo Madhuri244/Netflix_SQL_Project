d69142dca060df9188a7031e861a82bacb25fe83
Madhuri244 <madhuri.yarlag@gmail.com>
2025-09-13 21:48:16 -0400

    Add files via upload

---
+++ Schemas.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS netflix;
+
+CREATE table NETFLIX(
+show_id  VARCHAR(500),
+type VARCHAR(500),
+title VARCHAR(500),
+director VARCHAR(500),
+country VARCHAR(500),
+date_added VARCHAR(5),
+release_year INT,
+rating VARCHAR(250),
+duration VARCHAR(250),
+listed_in VARCHAR(250)
+);
